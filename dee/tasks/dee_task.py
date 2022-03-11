04fb8c81cbedac9d75344715dfda1dce6bb168b9
Qinyan Li <ga92fej@mytum.de>
2022-03-11 22:12:34 +0100

    aa

--- dee/tasks/dee_task.py
+++ dee/tasks/dee_task.py
@@ -262,6 +262,12 @@ class DEETaskSetting(TaskSetting):
             self.inference_file_name = "luge_submit_without_trigger_short.json"
             #self.inference_file_name = "luge_dev_without_trigger.json"
             self.doc_lang = "zh"
+        elif self.run_mode =="luge_without_trigger_multi_role":
+            self.train_file_name = "luge_train_without_trigger_multirole.json"
+            self.dev_file_name = "luge_dev_without_trigger_multirole.json"
+            self.test_file_name = "luge_dev_without_trigger_multirole.json"
+            self.inference_file_name = "luge_submit_without_trigger_multirole.json"
+            self.doc_lang = "zh"
         elif self.run_mode == "luge_with_trigger":
             self.train_file_name = "luge_train_with_trigger.json"
             self.dev_file_name = "luge_dev_with_trigger.json"
