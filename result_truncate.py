81a5b3b546db5cce12ccf418e82742b630354271
Ubuntu <ubuntu@ip-172-31-43-196.eu-central-1.compute.internal>
2022-03-12 15:44:30 +0000

    k

--- result_truncate.py
+++ result_truncate.py
@@ -1,7 +1,7 @@
 import json
 
 # convert into luge submit format
-with open(r'./luge_p1_submit_new_mlp_multi_roles_0310.json', "r", encoding="utf-8") as f:
+with open(r'./luge_p1_submit_new_mlp_0312.json', "r", encoding="utf-8") as f:
     json_data = []
     for line in f.readlines():
         json_data.append(json.loads(line))
@@ -12,7 +12,7 @@ with open(r'./luge_p1_submit_new_mlp_multi_roles_0310.json', "r", encoding="utf-
         i.pop("sentences",0)
         list1.append(i)
 
-with open(r'./luge_submit_multirole_truncated_0310.json', "w", encoding="UTF-8") as e:
+with open(r'./luge_submit_mlp_truncated_0312.json', "w", encoding="UTF-8") as e:
     
     # json_new_data = json.dumps(list1, ensure_ascii=False, indent=4)
     for line in list1:
